117a9025dc7196f8edcaa5176d83a1519b7bcfd1
drauger <drauger@yandex.ru>
2024-05-13 13:20:58 +0300

    Add files via upload

--- keyboards/sofle_drauger/keymaps/via/config.h
+++ keyboards/sofle_drauger/keymaps/via/config.h
@@ -1,8 +1,16 @@
-// Copyright 2024 Santosh Kumar (@santosh)
-// SPDX-License-Identifier: GPL-2.0-or-later
 
-#pragma once
+// #define TRI_LAYER_LOWER_LAYER 1
+// #define TRI_LAYER_UPPER_LAYER 2
+// #define TRI_LAYER_ADJUST_LAYER 3
 
-#define TRI_LAYER_LOWER_LAYER 2
-#define TRI_LAYER_UPPER_LAYER 3
-#define TRI_LAYER_ADJUST_LAYER 4
+// #ifdef TAPPING_TOGGLE
+// #undef TAPPING_TOGGLE
+// #define TAPPING_TOGGLE 2
+// #endif
+
+// #ifdef ONESHOT_TAP_TOGGLE
+// #undef ONESHOT_TAP_TOGGLE
+// #define ONESHOT_TAP_TOGGLE 2
+// #endif
+
+#define TAPPING_TERM 200
