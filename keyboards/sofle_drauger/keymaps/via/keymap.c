37adef0fed48bb96374997f5e480b37bcd465b54
drauger <drauger@yandex.ru>
2024-06-03 11:32:57 +0300

    Update keymap.c

--- keyboards/sofle_drauger/keymaps/via/keymap.c
+++ keyboards/sofle_drauger/keymaps/via/keymap.c
@@ -45,11 +45,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [0] = LAYOUT(
-   KC_ESC, KC_1,  KC_2,    KC_3,  KC_4,  KC_5,                    KC_6,  KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
-   KC_TAB, KC_Q,  KC_W,    KC_E,  KC_R,  KC_T,                    KC_Y,  KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
-  KC_LGUI, KC_A,  KC_S,    KC_D,  KC_F,  KC_G,                    KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,  KC_X,    KC_C,  KC_V,  KC_B, KC_LNG1, KC_LNG2,  KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-               KC_LCTL, KC_LALT, MO(1), MO(2),  KC_SPC,  KC_ENT, MO(2), MO(1), KC_RALT, KC_RCTL
+   KC_ESC, KC_1,  KC_2,    KC_3,  KC_4,  KC_5,                        KC_6,  KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
+   KC_TAB, KC_Q,  KC_W,    KC_E,  KC_R,  KC_T,                        KC_Y,  KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
+  KC_LGUI, KC_A,  KC_S,    KC_D,  KC_F,  KC_G,                        KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,  KC_X,    KC_C,  KC_V,  KC_B, LSA(KC_1), LSA(KC_2),  KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+               KC_LCTL, KC_LALT, MO(1), MO(2),    KC_SPC,    KC_ENT, MO(2), MO(1), KC_RALT, KC_RCTL
 ),
 /*
  * LOWER - NUM
@@ -209,22 +209,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         // case KC_GT:
         // case KC_RPRN:
         // case KC_DQT:
-        case KC_LNG1:
+        case KC_1:
         {
-            oled_set_cursor(0, 13);
-		    if (record->event.pressed) {
-            //     oled_write_ln_P(PSTR("En"), false);
-            // } else {
-            //     oled_write_ln_P(PSTR(""), false);
+            if (mod_state & MOD_MASK_SA) {
+                oled_set_cursor(0, 13);
+                if (record->event.pressed) {
+                    oled_write_ln_P(PSTR("En"), false);
+                // } else {
+                //     oled_write_ln_P(PSTR(""), false);
+                }
             }
-        case KC_LNG2:
+        return true;
+        }
+        case KC_2:
         {
-            oled_set_cursor(0, 13);
-		    if (record->event.pressed) {
-            //     oled_write_ln_P(PSTR("Ru"), false);
-            // } else {
-            //     oled_write_ln_P(PSTR(""), false);
+            if (mod_state & MOD_MASK_SA) {
+                oled_set_cursor(0, 13);
+                if (record->event.pressed) {
+                    oled_write_ln_P(PSTR("Ru"), false);
+                // } else {
+                //     oled_write_ln_P(PSTR(""), false);
+                }
             }
+        return true;
+        }
     }
     return true;
 };
