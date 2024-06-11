67171fae5e3a5b8e3025c2057ebcf057b4cce421
drauger <drauger@yandex.ru>
2024-06-11 18:18:23 +0300

    Update keymap.c

--- keyboards/sofle_drauger/keymaps/via/keymap.c
+++ keyboards/sofle_drauger/keymaps/via/keymap.c
@@ -122,6 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 static uint8_t lang;
 static uint8_t mod_state;
+static RGB rgb;
 
 void switchLanguage(void) {
     clear_mods();
